115595ee34ab492030d6a34843fa45d5aa2b32c8
bkecskemeti <balazs.kecskemeti@gmail.com>
2018-01-06 01:57:15 +0100

    small fix

--- IK_debug.py
+++ IK_debug.py
@@ -94,6 +94,8 @@ def test_code(test_case):
     T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
     T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
 
+    T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+
     # EE position and orientation
     px = req.poses[x].position.x
     py = req.poses[x].position.y
